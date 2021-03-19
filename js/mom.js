0d502784a4935418a776c33f1eca97c282fef9b8
vidushig08 <60618677+vidushig08@users.noreply.github.com>
2021-03-19 13:56:56 +0530

    mom form

---
+++ js/mom.js
@@ -0,0 +1,19 @@
+document.getElementById("addpoint").onclick = function() {
+  document.getElementById("point2").style.display = "inline";
+  document.getElementById("p2").style.display = "inline";
+}
+
+
+/*
+document.getElementById("addpoint").onclick = function() {
+  document.getElementById("point3").style.display = "inline";
+  document.getElementById("p3").style.display = "inline";
+}
+
+
+function addPoint() {
+  var c = document.getElementById("addpoints").children;
+  c[1].style.backgroundColor = "yellow";
+}
+
+*/
