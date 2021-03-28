e1436df45d0abb763a55c44eb61e5d7842eaaaf6
vidushig08 <vidushi.gupta2019@vitstudent.ac.in>
2021-03-28 16:15:19 +0530

    mom page

--- js/mom.js
+++ js/mom.js
@@ -1,19 +1,13 @@
+/*
 document.getElementById("addpoint").onclick = function() {
   document.getElementById("point2").style.display = "inline";
   document.getElementById("p2").style.display = "inline";
 }
-
-
-/*
-document.getElementById("addpoint").onclick = function() {
-  document.getElementById("point3").style.display = "inline";
-  document.getElementById("p3").style.display = "inline";
-}
-
-
-function addPoint() {
-  var c = document.getElementById("addpoints").children;
-  c[1].style.backgroundColor = "yellow";
-}
-
 */
+
+var btn=document.getElementById("addpoint");
+btn.addEventListener("click",add);
+
+function add() {
+  console.log("hello");
+};
