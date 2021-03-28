2580c32865d7663ec01b740020dcaf0c9ca6c104
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-03-28 19:11:56 +0530

    members bare-bones setup

---
+++ js/members.js
@@ -0,0 +1,24 @@
+const memberList = document.getElementById("members");
+
+const getUsers = async () => {
+  try {
+    let users = await fetch(
+      "https://internals-app-c0391.firebaseio.com/Users.json"
+    );
+    users = await users.json();
+    // console.log(users);
+    return users;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// const renderUsers = async () => {
+//   var userList = await getUsers();
+//   userList = Object.values(userList);
+//   console.log(userList);
+
+//   for (let i = 0; i < userList.length; ++i) {
+//     memberList.innerHTML += `${userList[i].name} <br />`;
+//   }
+// };
