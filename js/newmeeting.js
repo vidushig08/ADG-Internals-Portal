b5ec91ee99c36afe2d659da0711ffb275ec9b971
anmolbansal7 <abanmolbansal5@gmail.com>
2021-04-08 12:39:08 +0530

    new meetings

--- js/newmeeting.js
+++ js/newmeeting.js
@@ -47,6 +47,61 @@
     $("#newMeetingForm").submit(function(e) {
       e.preventDefault();
     });
+
+
+
+//Validate checkbox
+function handleData1()
+{
+    var form_data = new FormData(document.querySelector("form"));
+    if(!form_data.has("t[]"))
+    {
+      alert('Select a team first');
+      return false;
+    }
+    else
+    {
+      //document.getElementById("chk_option_error").style.visibility = "hidden";
+      console.log("hehe");
+      openModal();
+      return true;
+    }
+}
+
+function openModal(){
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+  modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+}
+
+
+
+
+
+
+
+
     
 //Fetching Data in a table in Modal
 
@@ -57,6 +112,7 @@ function SelectAllData(){
       function(CurrentRecord){
         var member = CurrentRecord.val().name;
         var meetuserid = CurrentRecord.val().uid;
+        var teamId
         AddItemsToTable(member, meetuserid);
       }
     );
@@ -70,12 +126,12 @@ function AddItemsToTable(member, meetuserid){
   var table = document.getElementById('memberslist');
   var trow = document.createElement('tr');
   //Changed from Label to th
-  var td1 = document.createElement('th');
+  var td1 = document.createElement('td');
   td1.innerHTML = member;
   var x = document.createElement("INPUT");
   console.log(x);
   x.setAttribute("type", "checkbox");
-  //x.setAttribute("id", "human");
+  x.setAttribute("class", "human");
   x.setAttribute("data-value", meetuserid);
   console.log("5");
   console.log(meetuserid);
@@ -112,7 +168,7 @@ function searchTable() {
   table = document.getElementById("memberslist");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
-    th = tr[i].getElementsByTagName("th")[0];
+    th = tr[i].getElementsByTagName("td")[0];
     if (th) {
       txtValue = th.textContent || th.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -121,5 +177,25 @@ function searchTable() {
         tr[i].style.display = "none";
       }
     }       
+  }
+}
+
+
+function checkAll(ele) {
+  //var checkboxes = document.getElementsByTagName('input');
+  var checkboxes = document.getElementsByClassName('human');
+  if (ele.checked) {
+      for (var i = 0; i < checkboxes.length; i++) {
+          if (checkboxes[i].type == 'checkbox') {
+              checkboxes[i].checked = true;
+          }
+      }
+  } else {
+      for (var i = 0; i < checkboxes.length; i++) {
+          // console.log(i)
+          if (checkboxes[i].type == 'checkbox') {
+              checkboxes[i].checked = false;
+          }
+      }
   }
 }
