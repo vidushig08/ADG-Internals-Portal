d67ae9c3f6cbd174e279aafe858541e0d46fcd45
vidushig08 <vidushi.gupta2019@vitstudent.ac.in>
2021-04-09 19:47:46 +0530

    Meetings Page Firebase Done

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,201 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+//Validate radio button
+function handleData1()
+{
+    var form_data = new FormData(document.querySelector("form"));
+    if(!form_data.has("t[]"))
+    {
+      alert('Select a team first');
+      return false;
+    }
+    else
+    {
+      //document.getElementById("chk_option_error").style.visibility = "hidden";
+      console.log("hehe");
+      openModal();
+      return true;
+    }
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+//Get radio button value
+  function getCheckedValue(el) {
+    for (var i = 0, length = el.length; i < length; i++) {
+      if (el[i].checked) {
+        return el[i].value;
+        break;
+      }
+    }
+    return '';
   }
+  
+  //Open Modal
+  function openModal()
+  {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+    sortTeam();
+  }
+
+
+  //Fetch Data according to team (Core/Team ID)
+  function sortTeam(){
+    var chosenCore = getCheckedValue(document.getElementsByName('t[]'));
+    if (chosenCore=="z")
+    {
+      selectAllDataCore();
+    }
+    else if (chosenCore == 0||1||2||3||4||5||6||7||8) {
+      selectAllData();
+    }
+  }
+
+  //Fetching ALL Member Names in the Modal
+  function selectAllDataCore(){
+      firebase.database().ref('Users').once('value',
+      function(AllRecords){
+        AllRecords.forEach(
+          function(CurrentRecord){
+            var member = CurrentRecord.val().name;
+            var meetuserid = CurrentRecord.val().uid;
+            AddItemsToTable(member, meetuserid);
+        }
+      );
+    });
+  }
+
+  //Fetching Data in a table in Modal
+    function selectAllData(){
+      var chosenTeam = getCheckedValue(document.getElementsByName('t[]'));
+      console.log(chosenTeam);
+      firebase.database().ref('Users').once('value',
+      function(AllRecords){
+        AllRecords.forEach(
+          function(CurrentRecord){
+            var member = CurrentRecord.val().name;
+            var meetuserid = CurrentRecord.val().uid;
+            var teamId = CurrentRecord.val().teams;
+            console.log(teamId);
+            var n = teamId.includes(parseInt(chosenTeam));
+            if (n==true)
+            {
+            console.log("hi");
+            AddItemsToTable(member, meetuserid);
+            }
+            else{
+              console.log("bye");
+              //enter something :)
+            }
+          }
+        );
+      });
+    }
+    // window.onload = selectAllData;
+
+  //Filling the table in the Modal
+  function AddItemsToTable(member, meetuserid){
+    var table = document.getElementById('memberslist');
+    var trow = document.createElement('tr');
+    //Changed from Label to th
+    var td1 = document.createElement('td');
+    td1.innerHTML = member;
+    var x = document.createElement("INPUT");
+    console.log(x);
+    x.setAttribute("type", "checkbox");
+    x.setAttribute("class", "human");
+    x.setAttribute("name", "item[]");
+    x.setAttribute("data-value", meetuserid);
+    x.setAttribute("value", member);
+    console.log("5");
+    console.log(meetuserid);
+    trow.appendChild(x);
+    trow.appendChild(td1);
+    table.appendChild(trow);
+  }
+
+//Search bar in Modal
+function searchTable() {
+var input, filter, table, tr, th, i, txtValue;
+input = document.getElementById("searchBar");
+filter = input.value.toUpperCase();
+table = document.getElementById("memberslist");
+tr = table.getElementsByTagName("tr");
+for (i = 0; i < tr.length; i++) {
+  th = tr[i].getElementsByTagName("td")[0];
+  if (th) {
+    txtValue = th.textContent || th.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
+    }
+  }       
 }
+}
+
+//To display names of chosen members
+function test(){
+var meetuserArr = [];
+  $("input[type='checkbox']").each(function(index, el) {
+    if (el.checked) {
+      var val = $(el).data("value");
+      meetuserArr.push(val);
+      return meetuserArr;
+    }
+  });
+  console.log(meetuserArr);
+
+
+  var nameuserArr = $("input[name='item[]']:checked").map(function () {
+    return this.value;
+  }).get();
+  console.log(nameuserArr);
+
+  nameuserArr.forEach(el => {
+       document.getElementById('selectedMembers').innerHTML +=`<button class="pill">${el}</button>`;
+       // here result is the id of the div present in the dom
+    });
+
+  var meetteamArr = [];
+    $("input[type='radio']").each(function(index, el) {
+      if (el.checked) {
+        var val = $(el).data("value");
+        meetteamArr.push(val);
+      }
+    });
+    console.log(meetteamArr);
+  };
+
+
+ //Select All Checkbox
+ $(document).ready(function () {
+  $("#checkAll").change(function () {
+    $("input.human").prop('checked', $(this).prop("checked"));
+  });
+  $('.human').on('click', function () {
+    if ($('.human:checked').length == $('.human').length) {
+      $('#checkAll').prop('checked', true);
+    } else {
+      $('#checkAll').prop('checked', false);
+    }
+  });
+});
--- js/newmeeting.js
+++ js/newmeeting.js
@@ -16,206 +16,7 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-  //document.getElementById("post-meeting-btn").addEventListener('submit', readData)
-
-  //Validate radio button
-  function handleData1()
-  {
-      var form_data = new FormData(document.querySelector("form"));
-      if(!form_data.has("t[]"))
-      {
-        alert('Select a team first');
-        return false;
-      }
-      else
-      {
-        //document.getElementById("chk_option_error").style.visibility = "hidden";
-        console.log("hehe");
-        openModal();
-        return true;
-      }
-  }
-
-  
-  //Get radio button value
-    function getCheckedValue(el) {
-      for (var i = 0, length = el.length; i < length; i++) {
-        if (el[i].checked) {
-          return el[i].value;
-          break;
-        }
-      }
-      return '';
-    }
-    
-
-    //Open Modal
-    function openModal()
-    {
-      // Get the modal
-      var modal = document.getElementById("myModal");
-
-      // Get the button that opens the modal
-      var btn = document.getElementById("myBtn");
-
-      // Get the <span> element that closes the modal
-      var span = document.getElementsByClassName("close")[0];
-
-      // When the user clicks the button, open the modal 
-      modal.style.display = "block";
-
-      // When the user clicks on <span> (x), close the modal
-      span.onclick = function() {
-      modal.style.display = "none";
-      }
-
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
-      selectAllData();
-    }
-
-    //Fetching Data in a table in Modal
-      function selectAllData(){
-        var chosenTeam = getCheckedValue(document.getElementsByName('t[]'));
-        console.log(chosenTeam);
-        firebase.database().ref('Users').once('value',
-        function(AllRecords){
-          AllRecords.forEach(
-            function(CurrentRecord){
-              var member = CurrentRecord.val().name;
-              var meetuserid = CurrentRecord.val().uid;
-              var teamId = CurrentRecord.val().teams;
-              console.log(teamId);
-              var n = teamId.includes(parseInt(chosenTeam));
-              if (n==true)
-              {
-              console.log("hi");
-              AddItemsToTable(member, meetuserid);
-              }
-              else{
-                console.log("bye");
-                //enter something :)
-              }
-            }
-          );
-        });
-      }
-      // window.onload = selectAllData;
-
-    //Filling the table in the Modal
-    function AddItemsToTable(member, meetuserid){
-      var table = document.getElementById('memberslist');
-      var trow = document.createElement('tr');
-      //Changed from Label to th
-      var td1 = document.createElement('td');
-      td1.innerHTML = member;
-      var x = document.createElement("INPUT");
-      console.log(x);
-      x.setAttribute("type", "checkbox");
-      x.setAttribute("class", "human");
-      x.setAttribute("name", "item[]");
-      x.setAttribute("data-value", meetuserid);
-      x.setAttribute("value", member);
-      console.log("5");
-      console.log(meetuserid);
-      trow.appendChild(x);
-      trow.appendChild(td1);
-      table.appendChild(trow);
-    }
-
-
-//Search bar in Modal
-function searchTable() {
-  var input, filter, table, tr, th, i, txtValue;
-  input = document.getElementById("searchBar");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("memberslist");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    th = tr[i].getElementsByTagName("td")[0];
-    if (th) {
-      txtValue = th.textContent || th.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
-}
-
-//Select All Checkboxes in Modal
-/*
-function checkAll(ele) {
-  //var checkboxes = document.getElementsByTagName('input');
-  var checkboxes = document.getElementsByClassName('human');
-  if (ele.checked) {
-      for (var i = 0; i < checkboxes.length; i++) {
-          if (checkboxes[i].type == 'checkbox') {
-              checkboxes[i].checked = true;
-          }
-      }
-  } else {
-      for (var i = 0; i < checkboxes.length; i++) {
-          // console.log(i)
-          if (checkboxes[i].type == 'checkbox') {
-              checkboxes[i].checked = false;
-          }
-      }
-  }
-}
-
-*/
-
-//To display names of chosen members
-function test(){
-  var meetuserArr = [];
-  // var nameuserArr = [];
-    $("input[type='checkbox']").each(function(index, el) {
-      if (el.checked) {
-        var val = $(el).data("value");
-        //var mname = $(el).data("value");
-        meetuserArr.push(val);
-        //nameuserArr.push(mname);
-      }
-    });
-    console.log(meetuserArr);
-
-   /*var nameuserArr = [];
-    $("input[type='checkbox']").each(function(index, el) {
-      if (el.checked) {
-        var mname = $(el).data("value");
-        nameuserArr.push(mname);
-      }
-    });
-    console.log(nameuserArr);*/
-
-    var nameuserArr = $("input[name='item[]']:checked").map(function () {
-      return this.value;
-    }).get();
-    console.log(nameuserArr);
-
-    nameuserArr.forEach(el => {
-         document.getElementById('selectedMembers').innerHTML +=`<button class="pill">${el}</button>`;
-         // here result is the id of the div present in the dom
-      });
-
-      var meetteamArr = [];
-    $("input[type='radio']").each(function(index, el) {
-      if (el.checked) {
-        var val = $(el).data("value");
-        meetteamArr.push(val);
-      }
-    });
-    console.log(meetteamArr);
-   };
-
-    
-
+  document.getElementById("post-meeting-btn").addEventListener('submit', readData)
 
 //To read values of the form
 function readData() {
@@ -227,24 +28,55 @@ function readData() {
   let link = document.getElementById("link").value;
   let chosenTeam = getCheckedValue(document.getElementsByName('t[]'));
   console.log(chosenTeam);
-  var type1 = "Duties";
+  var pushmeetuserArr = [];
+  // var nameuserArr = [];
+    $("input[type='checkbox']").each(function(index, el) {
+      if (el.checked) {
+        var val = $(el).data("value");
+        //var mname = $(el).data("value");
+        pushmeetuserArr.push(val);
+        //nameuserArr.push(mname);
+      }
+    });
+  //let type1 = "Team ID";
 
-  writeUserData(unixdate,link,venue,title,type1);
+  writeUserData(unixdate,link,venue,title,chosenTeam,pushmeetuserArr);
   }
 
   //To push values to firebase
-  function writeUserData(unixdate,link,venue,title,type1){
-    console.log("3");
-    var newMeetingKey = firebase.database().ref().child('Alerts/Core/').push().key;
-    firebase.database().ref('Alerts/Core/').push({
-    id: newMeetingKey,
-    date: unixdate,
-    title: title,
-    location: venue, 
-    link: link,
-    type: type1
-  });
+  function writeUserData(unixdate,link,venue,title,chosenTeam,pushmeetuserArr)
+  {
+    if (chosenTeam == "z")
+    {
+      var meetingCore = "Meetings";
+      var newMeetingKey = firebase.database().ref().child('Alerts/Core/').push().key;
+      firebase.database().ref('Alerts/Core/').push({
+      id: newMeetingKey,
+      date: unixdate,
+      title: title,
+      location: venue, 
+      link: link,
+      type: meetingCore,
+      users: pushmeetuserArr
+    });
+    }
+
+    else if (chosenTeam == 0||1||2||3||4||5||6||7||8)
+    {
+      var newMeetingKey = firebase.database().ref().child('Alerts/Team/').push().key;
+      firebase.database().ref('Alerts/Team/').push({
+      id: newMeetingKey,
+      date: unixdate,
+      title: title,
+      location: venue, 
+      link: link,
+      type: chosenTeam,
+      users: pushmeetuserArr
+    });
+    }
+    alert("Submitted");
   }
+
 
   //Prevent form from refreshing on submit
   $("#newMeetingForm").submit(function(e) {
@@ -252,15 +84,3 @@ function readData() {
   });
 
 
-  $(document).ready(function () {
-    $("#checkAll").change(function () {
-      $("input.human").prop('checked', $(this).prop("checked"));
-    });
-    $('.human').on('click', function () {
-      if ($('.human:checked').length == $('.human').length) {
-        $('#checkAll').prop('checked', true);
-      } else {
-        $('#checkAll').prop('checked', false);
-      }
-    });
-  });
