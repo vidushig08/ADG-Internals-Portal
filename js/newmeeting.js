68868ee00747b518b6ee2b96654d75234b26b472
anmolbansal7 <abanmolbansal5@gmail.com>
2021-04-13 22:48:27 +0530

    Some code

--- js/newmeeting.js
+++ js/newmeeting.js
@@ -1,21 +1,3 @@
-// Your web app's Firebase configuration
-
-//const { default: firebase } = require("firebase");
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyAgHtxEJqKVsXItchYAZ8pvCyR38ReYhzQ",
-    authDomain: "internals-app-c0391.firebaseapp.com",
-    databaseURL: "https://internals-app-c0391.firebaseio.com",
-    projectId: "internals-app-c0391",
-    storageBucket: "internals-app-c0391.appspot.com",
-    messagingSenderId: "754737704023",
-    appId: "1:754737704023:web:5ec000ba7b9d08cea48712",
-    measurementId: "G-YYZML2JL2J"
-  };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 document.getElementById("newMeetingForm").addEventListener('submit', TDate)
 
 //Date Checker to be Greater than Today's
