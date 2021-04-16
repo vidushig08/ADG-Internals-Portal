706c8de0bf163804df22613e7236a41a649e18e3
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-16 22:17:07 +0530

    scheduled meeting users ✅

--- js/scheduledmeeting.js
+++ js/scheduledmeeting.js
@@ -45,4 +45,57 @@ const renderMeetings = async () => {
   // fetchedMeetings.innerHTML += TeamsMeetings + CoreMeetings;
 };
 
+let id;
+
+const selectMeeting = async (meetingId) => {
+  id = meetingId;
+  try {
+    console.log(meetingId);
+    const dbRef = firebase.database().ref();
+    let attendance = await dbRef
+      .child("AlertAttendance")
+      .child(meetingId)
+      .get();
+    let snapshot = await attendance.val();
+
+    let Users = await Promise.all(
+      Object.entries(snapshot).map(async ([key, value]) => {
+        let user = await dbRef.child("Users").child(key).get();
+        let snap = await user.val();
+        console.log(snap);
+        return {
+          name: snap.name,
+          regNo: snap.regNo,
+          reason: value,
+        };
+      })
+    );
+    console.log(Users);
+
+    const ackMarkup = document.querySelector(".available");
+
+    let acknowledged = Users.filter((u) => u.reason === "available");
+
+    console.log(ackMarkup);
+    ackMarkup.innerHTML = `<h3>Acknowledged</h3>`;
+    acknowledged.forEach((user) => {
+      ackMarkup.innerHTML += `<div class="user">
+        <h4>${user.name}</h4>
+        <p>${user.regNo}</p>
+      </div>`;
+    });
+    let unavailable = Users.filter((u) => u.reason !== "available");
+    const nonAckMarkup = document.querySelector(".unavailable");
+    console.log(nonAckMarkup);
+    nonAckMarkup.innerHTML = `<h3>Unavailable</h3>`;
+    unavailable.forEach((user) => {
+      nonAckMarkup.innerHTML += `<div class="user">
+        <h4>${user.name}</h4>
+        <p>${user.regNo}</p>
+      </div>`;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 renderMeetings();
