06d946f83148879c155b2a879041aa80ff743d70
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-27 16:23:57 +0530

    ✨ Scheduled Meetings

--- js/scheduledmeeting.js
+++ js/scheduledmeeting.js
@@ -46,10 +46,15 @@ const renderMeetings = async () => {
 };
 
 let id;
-
-const selectMeeting = async (meetingId) => {
-  id = meetingId;
+var Users;
+let meetingId;
+let unavailable;
+let acknowledged;
+const selectMeeting = async (mId) => {
+  meetingId = mId;
   try {
+    const ackMarkup = document.querySelector(".available");
+    const nonAckMarkup = document.querySelector(".unavailable");
     console.log(meetingId);
     const dbRef = firebase.database().ref();
     let attendance = await dbRef
@@ -57,45 +62,71 @@ const selectMeeting = async (meetingId) => {
       .child(meetingId)
       .get();
     let snapshot = await attendance.val();
+    console.log(snapshot);
 
-    let Users = await Promise.all(
+    Users = await Promise.all(
       Object.entries(snapshot).map(async ([key, value]) => {
         let user = await dbRef.child("Users").child(key).get();
         let snap = await user.val();
         console.log(snap);
-        return {
-          name: snap.name,
-          regNo: snap.regNo,
-          reason: value,
-        };
+        return { ...snap };
       })
     );
     console.log(Users);
 
-    const ackMarkup = document.querySelector(".available");
-
-    let acknowledged = Users.filter((u) => u.reason === "available");
+    acknowledged = Users.filter((u) => snapshot[u.uid] === "available");
 
     console.log(ackMarkup);
     ackMarkup.innerHTML = `<h3>Acknowledged</h3>`;
-    acknowledged.forEach((user) => {
-      ackMarkup.innerHTML += `<div class="user">
-        <h4>${user.name}</h4>
-        <p>${user.regNo}</p>
-      </div>`;
-    });
-    let unavailable = Users.filter((u) => u.reason !== "available");
-    const nonAckMarkup = document.querySelector(".unavailable");
+    if (acknowledged) {
+      acknowledged.forEach((user, i) => {
+        ackMarkup.innerHTML += `<div class="user">
+          <h4>${user.name}</h4>
+          <p>${user.regNo}</p>
+          <div id="${i}"onclick="changeStatus(this.id, 0)">Change Status</div>
+        </div>`;
+      });
+    } else {
+      nonAckMarkup.innerHTML = `<p>No members</p>`;
+    }
+    unavailable = Users.filter((u) => snapshot[u.uid] !== "available");
     console.log(nonAckMarkup);
     nonAckMarkup.innerHTML = `<h3>Unavailable</h3>`;
-    unavailable.forEach((user) => {
-      nonAckMarkup.innerHTML += `<div class="user">
-        <h4>${user.name}</h4>
-        <p>${user.regNo}</p>
-      </div>`;
-    });
+    if (unavailable) {
+      unavailable.forEach((user, i) => {
+        nonAckMarkup.innerHTML += `<div class="user">
+          <h4>${user.name}</h4>
+          <p>${user.regNo}</p>
+          <div id="${i}"onclick="changeStatus(this.id, 1)">Change Status</div>
+        </div>`;
+      });
+    } else {
+      nonAckMarkup.innerHTML = `<p>No members</p>`;
+    }
   } catch (error) {
     console.error(error);
   }
 };
 renderMeetings();
+
+// Mark
+async function changeStatus(id, status) {
+  let update = {};
+  if ((status = 0)) {
+    let currUser = acknowledged[id];
+    let currUserId = currUser.uid;
+    update[currUserId] = "unavailable";
+  } else {
+    let currUser = unavailable[id];
+    let currUserId = currUser.uid;
+    update[currUserId] = "available";
+  }
+  const dbRef = firebase.database().ref();
+  try {
+    await dbRef.child("AlertAttendance").child(meetingId).update(update);
+
+    selectMeeting(meetingId);
+  } catch (error) {
+    console.error(error);
+  }
+}
