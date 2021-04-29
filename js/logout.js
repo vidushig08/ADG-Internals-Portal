13251fe390d797ac2dfd1f7e9342c0df437c8a21
anmolbansal7 <abanmolbansal5@gmail.com>
2021-04-29 19:28:37 +0530

    login removed for development

--- js/logout.js
+++ js/logout.js
@@ -1,17 +1,17 @@
-firebase.auth().onAuthStateChanged(function(user){
+/*firebase.auth().onAuthStateChanged(function(user){
   if(user){
       //user is signed in
   }else{
       window.location.assign("index.html");
   }
-});
+});*/
 
 function logout(){
     //window.location.replace("index.html");
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         alert("Signed Out");
-        //window.location.assign("index.html");
+        window.location.assign("index.html");
 
       }).catch((error) => {
         // An error happened.
